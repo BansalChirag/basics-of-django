8585fc050c69553995404f47aa500842b37fc89d
Chirag Bansal <bansalchirag091@gmail.com>
2025-07-26 14:35:43 +0530

    Add tutorial app with views, templates, and URL routing for chai page

---
+++ djangoTutorial/tutorial/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+# localhost:8000/chai
+urlpatterns = [
+    path('',views.all_chai,name='all_chai'), # name parameter is optional
+    
+]
