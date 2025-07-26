3e98a3a0eda44766b415591707f21c02ae47a26d
Chirag Bansal <bansalchirag091@gmail.com>
2025-07-26 13:48:29 +0530

    learned how to Add static file handling and update views to render templates

--- djangoTutorial/djangoTutorial/views.py
+++ djangoTutorial/djangoTutorial/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Hello world!")
+    # return HttpResponse("Hello world!")
+    return render(request,'index.html')
 
 def about(request):
     return HttpResponse("About Page")
